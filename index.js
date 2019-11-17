ed1c0b368de65fc338221daa78aac474a66d9bca
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-17 10:59:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ function wakeDog(){
   const b = "Border Collie"
   console.log(`Wake ${a} the ${b}`);
 }
+function leashDog(){
+  const a = "Boo Radley"
+  const b = "Pibble"
+  console.log(`Wake ${a} the ${b}`);
+}
 wakeDog();
