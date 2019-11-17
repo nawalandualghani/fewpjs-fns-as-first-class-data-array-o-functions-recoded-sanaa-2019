dbddb4d7950ead68a2a8e0c61addf2962f4423f0
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-17 11:52:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,41 +1,35 @@
-function wakeDog(){
-  const a = "Xerox"
-  const b = "Border Collie"
-  console.log(`Wake ${a} the ${b}`);
-}
-function leashDog(){
-  const a = "Boo Radley"
-  const b = "Pibble"
-  console.log(`Wake ${a} the ${b}`);
-}
-function walkToPark(){
-  const a = "Bunny"
-  const b = "Labrador"
-  console.log(`Wake ${a} the ${b}`);
-}
-function throwFrisbee(){
-  const a = "Mary J. Blige"
-  const b = "Papillon"
-  console.log(`Wake ${a} the ${b}`);
-}
-function walkHome(){
-  const a = "Santa Paws"
-  const b = "Husky"
-  console.log(`Wake ${a} the ${b}`);
-}
-function unleashDog(){
-  const a = "Lauren"
-  const b = "Golden Retriever"
-  console.log(`Wake ${a} the ${b}`);
+var wakeDog = function(dogName="Byron", dogBreed="poodle") {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Wake ${dogName} the ${dogBreed}`
 }
 
-// function wakeDog(dogName,dogBreed){
-//   console.log(`Wake ${dogName} the ${dogBreed}`);
-// }
-// wakeDog("Skippy","Welsh Corgi");
-function independent(){
-  let routine=[wakeDog(),leashDog(),walkToPark(),throwFrisbee(),walkHome(),unleashDog()];
-  for(let i=0; i<routine.lenght;i++){
-    console.log(`Wake ${i[0]} the ${i[1]}`);
-  }
+var leashDog = function(dogName="Byron", dogBreed="poodle") {
+  console.log(`Leash ${dogName} the ${dogBreed}`);
+  return `Leash ${dogName} the ${dogBreed}`
+}
+
+var walkToPark = function(dogName="Byron", dogBreed="poodle") {
+  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+  return `Walk to the park with ${dogName} the ${dogBreed}`
+}
+
+var throwFrisbee = function(dogName="Byron", dogBreed="poodle") {
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  return `Throw the frisbee for ${dogName} the ${dogBreed}`
+}
+
+var walkHome = function(dogName="Byron", dogBreed="poodle") {
+  console.log(`Walk home with ${dogName} the ${dogBreed}`);
+  return `Walk home with ${dogName} the ${dogBreed}`
+}
+
+var unleashDog = function(dogName="Byron", dogBreed="poodle") {
+  console.log(`Unleash ${dogName} the ${dogBreed}`);
+  return `Unleash ${dogName} the ${dogBreed}`
+}
+
+const routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]
+
+function exerciseDog(dog, breed) {
+  return routine.map(fn => fn(dog, breed))
 }
